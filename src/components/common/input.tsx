347e7c91f04369e4b8393963c6d4539bd63213cf
inshal1998 <inshalansari80@gmail.com>
2023-02-20 12:26:36 +0530

    feat:InputBox Added

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Keyboard, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { Colors, width } from '../../constants/constants'
 
-const Input = () => {
+interface Props{
+  placeholder:string,
+  onChangeText:(txt:string)=> void | undefined
+  secureTextEntry?:boolean
+}
+const Input:React.FC<Props> = (props) => {
   return (
-    <View>
-      <Text>Input</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text)=>{
+          console.log(text);
+          
+        }}
+        placeholder={props.placeholder}
+        placeholderTextColor={Colors.Black_Mist}
+        secureTextEntry={props.secureTextEntry}
+      />
     </View>
   )
 }
 
 export default Input
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    width:width/1.1,
+    alignSelf:'center',
+    backgroundColor:Colors.Platinum,
+    borderRadius:5,
+    marginVertical:10
+  },
+  input:{
+    padding:5,
+    color:Colors.Nature_Black
+  }
+})
